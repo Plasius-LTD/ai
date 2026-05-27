f3bc80600dd7dc73c1bcb8569d99512c1d8d0dfd
Phillip Hounslow <development@plasius.co.uk>
2026-05-27 16:18:07 +0100

    fix: localize pixelverse display text

--- src/components/pixelverse/index.ts
+++ src/components/pixelverse/index.ts
@@ -1,2 +1,4 @@
 export * from "./balance.js";
+export * from "./i18n.js";
+export * from "./translations/en-GB.js";
 export * from "./video-generation-editor.js";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./agentic-foundation.js";
 export * from "./platform/index.js";
 export * from "./lib/chatWithAI.js";
+export * from "./components/pixelverse/i18n.js";
+export * from "./components/pixelverse/translations/en-GB.js";
